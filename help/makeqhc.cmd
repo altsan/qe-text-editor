5dd25d32397381783fd7fffcec5d4599c31aa1c8
Alex <alex@altsan.org>
2021-11-07 11:56:30 -0500

    Add platform support to makeqhc

--- help/makeqhc.cmd
+++ help/makeqhc.cmd
@@ -1,6 +1,12 @@
 /* REXX script
  * Generate the QtAssistant collection and compressed help files.
  */
-uxroot = VALUE('UNIXROOT',,'OS2ENVIRONMENT')
-uxroot'\usr\lib\qt4\bin\qcollectiongenerator qe.qhcp -o qe.qhc'
+PARSE SOURCE _os .
+IF _os == 'OS/2' THEN DO
+    uxroot = VALUE('UNIXROOT',,'OS2ENVIRONMENT')
+    qcg_cmd = uxroot'\usr\lib\qt4\bin\qcollectiongenerator'
+END
+ELSE qcg_cmd = 'qcollectiongenerator'
 
+ADDRESS CMD qcg_cmd 'qe.qhcp -o qe.qhc'
+EXIT rc
